158b05ec335b97861794a435ef5125b50c225d0e
V <v.tyubek@gmail.com>
2015-12-02 22:59:30 +0300

    Updated entry script to handle any refs

--- entry.sh
+++ entry.sh
@@ -1,8 +1,12 @@
 #!/bin/sh
 set -e
 
-docker run --rm -v `pwd`:/data leanlabs/git-builder:latest -C $REPOSITORY_NAME pull || docker run --rm -v `pwd`:/data leanlabs/git-builder:latest clone $REPOSITORY_GIT_HTTP_URL $REPOSITORY_NAME
+if [ ! -d "$REPOSITORY_NAME" ]; then
+    mkdir -p $REPOSITORY_NAME
+fi
 
-docker run --rm -v `pwd`/$REPOSITORY_NAME:/data leanlabs/git-builder:latest checkout $AFTER
+docker run --rm -v `pwd`:/data leanlabs/git-builder:latest -C $REPOSITORY_NAME init
+docker run --rm -v `pwd`:/data leanlabs/git-builder:latest -C $REPOSITORY_NAME fetch $REPOSITORY_GIT_HTTP_URL $REF
+docker run --rm -v `pwd`:/data leanlabs/git-builder:latest -C $REPOSITORY_NAME checkout $AFTER
 
 docker run --rm -v `pwd`/$REPOSITORY_NAME:`pwd`/$REPOSITORY_NAME -w `pwd`/$REPOSITORY_NAME -v /var/run/docker.sock:/var/run/docker.sock -v /var/run/docker.pid:/var/run/docker.pid  leanlabs/make-builder
