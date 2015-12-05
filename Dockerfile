fdb51befe81f83a53b7a4deacb8cacc683979bbe
Vasiliy Tyubek <v.tyubek@gmail.com>
2015-12-05 23:35:35 +0300

    Updated runner to pass env variables to downstream builders

--- Dockerfile
+++ Dockerfile
@@ -3,7 +3,10 @@ FROM leanlabs/base-builder
 ENV REPOSITORY_GIT_HTTP_URL="myrepo" \
     REPOSITORY_NAME="reponame" \
     BEFORE="beforecommitid" \
-    AFTER="aftercommitid"
+    AFTER="aftercommitid" \
+    DOCKER_HUB_LOGIN=qwerty \
+    DOCKER_HUB_EMAIL=qwerty@qwerty.com \
+    DOCKER_HUB_PASSWORD=qwerty
 
 ADD https://get.docker.com/builds/Linux/x86_64/docker-1.9.1 /usr/bin/docker
 COPY ./entry.sh /entry.sh
--- entry.sh
+++ entry.sh
@@ -1,12 +1,35 @@
 #!/bin/sh
 set -e
 
+# Create empty directory to act on
 if [ ! -d "$REPOSITORY_NAME" ]; then
     mkdir -p $REPOSITORY_NAME
 fi
 
-docker run --rm -v `pwd`:/data leanlabs/git-builder:latest -C $REPOSITORY_NAME init
-docker run --rm -v `pwd`:/data leanlabs/git-builder:latest -C $REPOSITORY_NAME fetch $REPOSITORY_GIT_HTTP_URL $REF
-docker run --rm -v `pwd`:/data leanlabs/git-builder:latest -C $REPOSITORY_NAME checkout $AFTER
+# Initialize empty repository
+docker run --rm \
+    -v `pwd`:/data \
+    leanlabs/git-builder:latest \
+    -C $REPOSITORY_NAME init
 
-docker run --rm -v `pwd`/$REPOSITORY_NAME:`pwd`/$REPOSITORY_NAME -w `pwd`/$REPOSITORY_NAME -v /var/run/docker.sock:/var/run/docker.sock leanlabs/make-builder
+# Fetch git ref to from target repository to act on
+docker run --rm \
+    -v `pwd`:/data \
+    leanlabs/git-builder:latest \
+    -C $REPOSITORY_NAME fetch $REPOSITORY_GIT_HTTP_URL $REF
+
+# Checkout commit to act on
+docker run --rm \
+    -v `pwd`:/data \
+    leanlabs/git-builder:latest \
+    -C $REPOSITORY_NAME checkout $AFTER
+
+# Start build process
+docker run --rm \
+    -v `pwd`/$REPOSITORY_NAME:`pwd`/$REPOSITORY_NAME \
+    -w `pwd`/$REPOSITORY_NAME \
+    -v /var/run/docker.sock:/var/run/docker.sock \
+    -e DOCKER_HUB_EMAIL=$DOCKER_HUB_EMAIL \
+    -e DOCKER_HUB_LOGIN=$DOCKER_HUB_LOGIN \
+    -e DOCKER_HUB_PASSWORD=$DOCKER_HUB_PASSWORD \
+    leanlabs/make-builder
